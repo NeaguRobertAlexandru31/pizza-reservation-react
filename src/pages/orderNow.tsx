881ba453cbd1a7b952f9e79817497c8c6cd606a9
Neagu Robert Alexandru <127115019+NeaguRobertAlexandru31@users.noreply.github.com>
2024-03-11 16:01:44 +0100

    boh

--- src/pages/orderNow.tsx
+++ src/pages/orderNow.tsx
@@ -7,6 +7,8 @@ const OrderNow = () => {
   const [activeNavItem, setActiveNavItem] = useState<string>(navbarItems[0].label);
   const [isNavbarScrollLocked, setNavbarScrollLocked] = useState<boolean>(false);
   const [searchTerm, setSearchTerm] = useState<string>(''); 
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState<boolean>(false);
+
 
   const handleNavItemClick = (label: string) => {
     setActiveNavItem(label);
@@ -18,11 +20,24 @@ const OrderNow = () => {
 
   const SelectedPage = navbarItems.find((item) => item.label === activeNavItem)?.page;
 
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
 
     <section className='grid grid-cols-12 relative'>
-       <nav className="flex p-2 md:space-x-4 mb-8 bg-red-900 rounded-2xl m-4 col-span-12 md:col-span-9">
-        <div className='flex'>
+       <nav className="flex p-2 md:space-x-4 mb-8 bg-red-900 rounded-2xl m-4 col-span-12 md:col-span-9 justify-between">
+        {/* Icona dell'hamburger per la modalità mobile */}
+        <div className="md:hidden">
+          <button
+            className="text-white py-2 px-4 font-semibold rounded flex items-center cursor-pointer"
+            onClick={toggleMobileMenu}
+          >
+            <span className="mr-2">Icona Hamburger</span>
+          </button>
+        </div>
+        <div className='hidden md:flex w-[70%] justify-evenly md:w-full'>
         {navbarItems.map((item, index) => (
             <button
               key={index}
@@ -34,18 +49,36 @@ const OrderNow = () => {
             </button>
             ))}
         </div>
-          <div className=''>
-          <input
-              type="text"
-              placeholder="Cerca..."
-              className="p-2 md:px-4 rounded-xl"
-              value={searchTerm}
-              onChange={handleSearchChange}
-            />
+          <div className='flex w-[30%] justify-end col-span-12 md:flex md:w-auto'>
+            <input
+                type="text"
+                placeholder="Cerca..."
+                className="p-2 md:px-4 rounded-xl"
+                value={searchTerm}
+                onChange={handleSearchChange}
+              />
           </div>
-            
         </nav>
-        <nav className="flex justify-center p-2 space-x-4 mb-8 bg-red-900 rounded-2xl m-4 col-span-3 hidden md:flex">
+         {/* Menu dropdown per la modalità mobile */}
+         {isMobileMenuOpen && (
+          <div className="md:hidden bg-red-900 h-fit col-span-12 fixed top-[90px] p-2 mx-4 overflow-y-auto rounded-2xl">
+            <div className="flex flex-col items-center justify-self-start">
+              {navbarItems.map((item, index) => (
+                <button
+                  key={index}
+                  className={`text-white py-2 px-4 font-semibold rounded flex flex-row cursor-pointer ${item.label === activeNavItem ? "bg-white text-red-900" : ""}`}
+                  onClick={() => handleNavItemClick(item.label)}
+                >
+                  <span className="mr-2">{item.icon}</span>
+                  {item.label}
+                </button>
+              ))}
+            </div>
+            {/* Bottone per chiudere il menu dropdown */}
+            <button onClick={toggleMobileMenu}>Chiudi Menu</button>
+          </div>
+        )}
+        <nav className=" justify-center p-2 space-x-4 mb-8 bg-red-900 rounded-2xl m-4 col-span-3 hidden md:flex">
           <button className={`text-white py-2 px-4 md:px-2 font-semibold rounded flex items-center cursor-pointer`}>
             <span className="mr-2">cart</span>
           </button>
