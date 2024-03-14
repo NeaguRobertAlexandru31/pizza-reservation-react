6399c5d6855098ff3665f384c82ff574d0320262
neagu robert alexandru <127115019+NeaguRobertAlexandru31@users.noreply.github.com>
2024-03-14 22:55:03 +0100

    style: grid col & row

--- src/pages/orderNow.tsx
+++ src/pages/orderNow.tsx
@@ -2,104 +2,59 @@ import { useState } from 'react';
 /* Utils */
 import { navbarItems } from '../utils/constants';
 /* Components */
-import Cart from './carrelloPage';
+/* Icons */
 
 const OrderNow = () => {
 
+  /* useState */
   const [activeNavItem, setActiveNavItem] = useState<string>(navbarItems[0].label);
-  const [searchTerm, setSearchTerm] = useState<string>(''); 
-  const [isMobileMenuOpen, setMobileMenuOpen] = useState<boolean>(false);
 
-
+  /* useState functions */
   const handleNavItemClick = (label: string) => {
     setActiveNavItem(label);
   };
 
-  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(e.target.value);
-  };
-
+  /* Functions */
   const SelectedPage = navbarItems.find((item) => item.label === activeNavItem)?.page;
 
   return (
 
-    <section className ='grid grid-cols-12 relative'>
+    <section className ='grid grid-cols-12 grid-rows-10 h-[100vh]'>
       {/* Navbar Desktop */}
-      <nav className ="flex p-2 md:space-x-4 mb-8 bg-red-900 rounded-2xl m-4 col-span-12 md:col-span-9 justify-between">
-        {/* Logo Mobile */}
-        <div className ="md:hidden">
-          <button className ="text-white py-2 px-4 font-semibold rounded flex items-center cursor-pointer">
-            <span className ="mr-2">LOGO</span>
-          </button>
-        </div>
-        {/* End Logo Mobile */}
-
+      <nav className ="md:col-span-2 md:row-span-12 bg-blue-900 justify-between">
         {/* Navbar Items Desktop  */}
-        <div className='hidden md:flex w-[70%] justify-evenly md:w-full'>
+        <div className='flex flex-col justify-evenly md:p-2 md:m-2 md:mt-10'>
           {navbarItems.map((item, index) => (
             <button
-              className ={`text-white py-2 px-4 md:px-2 font-semibold rounded flex items-center cursor-pointer ${item.label === activeNavItem ? "bg-white text-red-800" : ""}`}
-              key       ={index}
-              onClick   ={() => handleNavItemClick(item.label)}
+              className = {`flex items-center md:py-2 md:px-2 text-white font-semibold rounded-lg cursor-pointer ${item.label === activeNavItem ? "bg-white text-blue-800" : ""}`}
+              key       = {index}
+              onClick   = {() => handleNavItemClick(item.label)}
             >
-            <span className="mr-2">{item.icon}</span>
+            <span className="m-2">{item.icon}</span>
               {item.label}
             </button>
             ))}
         </div>
         {/* End Navbar Items Desktop  */}
-
-        {/* Search Bar */}
-        <div className ='flex w-[30%] justify-end md:w-auto'>
-          <input
-            className   ="p-2 md:px-4 rounded-xl"           
-            type        ="text"
-            placeholder ="Cerca..."
-            value       ={searchTerm}
-            onChange    ={handleSearchChange}
-          />
-        </div>
-        {/* End Search Bar */}
       </nav>
       {/* End Navbar Desktop */}
-
-      {/* Bottom bar Mobile */}
-      <nav className ="md:hidden p-2 bg-red-900 rounded-2xl m-4 col-span-12 w-full fixed bottom-4 left-0">
-        <div className ="flex justify-between w-full">
-          {navbarItems.map((item, index) => (
-            <button
-              className ={`text-white py-2 px-4 font-semibold rounded-xl flex flex-row cursor-pointer ${item.label === activeNavItem ? "bg-white text-red-700" : ""}`}
-              key       ={index}
-              onClick   ={() => handleNavItemClick(item.label)}
-            >
-              <span className ="mr-2">{item.icon}</span>
-            </button>
-          ))}
+        <div className='flex justify-between items-center md:col-span-10 md:col-start-3 row-span-1 bg-blue-900 text-white'>
+            <div className='flex'>
+              
+            </div>
+            <div className='flex justify-center'>
+              <h1>searchbar</h1>
+            </div>
+            <div className='flex justify-end mr-2'>
+              <h1 className='mx-2'>profilo</h1>
+              <h1>carrello</h1>
+            </div>
         </div>
-      </nav>
-      {/* End Bottom Bar Mobile */}
-
-      {/* Cart Navbar */}
-      <nav className =" justify-center p-2 space-x-4 mb-8 bg-red-900 rounded-2xl m-4 col-span-3 hidden md:flex">
-        <button className ={`text-white py-2 px-4 md:px-2 font-semibold rounded flex items-center cursor-pointer`}>
-          <span className ="mr-2">cart</span>
-        </button>
-      </nav>
-      {/* End Cart Navbar */}
-
       {/* Pages */}
-      <div className ='col-span-12 md:col-span-9 h-full bg-gray-200 mx-4 rounded-2xl p-2 overflow-auto'>
+      <div className ='md:col-span-10 md:col-start-3 md:row-span-9 md:row-start-2 bg-gray-200 overflow-auto'>
         {SelectedPage && <SelectedPage />}
       </div>
       {/* End Pages */}
-
-      {/* Cart */}
-      <div className ='col-span-3 h-[85vh] bg-gray-200 mx-4 rounded-2xl p-2 hidden md:flex'>
-        <div className ='w-full h-full'>
-          <Cart />
-        </div>
-      </div>
-      {/* End Cart */}
     </section>
   );
 };
